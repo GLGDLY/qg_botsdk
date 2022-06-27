e69435b7b8cd9ad91004d5983d1223bdd49b2dab
GLGDLY <tzlgdly@gmail.com>
2022-06-27 18:06:04 +0800

    v2.2.5
    【优化】logg模块的写法
    【优化】register_time_event的重复函数唤起（选择异步时可用异步方式唤起函数）

--- qg_botsdk/logger.py
+++ qg_botsdk/logger.py
@@ -5,6 +5,7 @@ from os import mkdir
 from os.path import exists
 from time import strftime, localtime
 from inspect import stack
+from functools import wraps
 
 
 def __getattr__(identifier: str) -> object:
@@ -14,28 +15,43 @@ def __getattr__(identifier: str) -> object:
     return globals()[identifier.__path__]
 
 
+def _check_date(func):
+    @wraps(func)
+    def wrap(self, msg):
+        str_time = strftime('%m-%d', localtime())
+        if str_time != self._previous_time:
+            self._logger.removeHandler(self._logh)
+            self._new_logh(str_time)
+        try:
+            func(self, msg)
+        except UnicodeDecodeError:
+            pass
+
+    return wrap
+
+
 class Logger:
     def __init__(self, bot_app_id: str):
         for items in ["websockets.server", "websockets.protocol", "websockets.client", "asyncio"]:
             getLogger(items).disabled = True
-        self.__bot_app_id = bot_app_id
-        self.__logger = getLogger(__name__)
-        self.__logger.setLevel('DEBUG')
+        self._bot_app_id = bot_app_id
+        self._logger = getLogger(__name__)
+        self._logger.setLevel('DEBUG')
         self._format = '[%(asctime)s] [%(levelname)s] %(message)s'
         self._date_format = '%m-%d %H:%M:%S'
-        self.__cmdh = StreamHandler()
-        self.__cmdh.setFormatter(self.__Stream_Formatter())
-        self.__cmdh.setLevel('INFO')
+        self._cmdh = StreamHandler()
+        self._cmdh.setFormatter(self._Stream_Formatter())
+        self._cmdh.setLevel('INFO')
         if not exists('./log'):
             mkdir('./log')
-        if not exists(f'./log/{self.__bot_app_id}'):
-            mkdir(f'./log/{self.__bot_app_id}')
-        self.__logh = None
-        self.__new_logh(strftime('%m-%d', localtime()))
-        self.__logger.addHandler(self.__cmdh)
-        self.__previous_time = strftime('%m-%d', localtime())
+        if not exists(f'./log/{self._bot_app_id}'):
+            mkdir(f'./log/{self._bot_app_id}')
+        self._logh = None
+        self._new_logh(strftime('%m-%d', localtime()))
+        self._logger.addHandler(self._cmdh)
+        self._previous_time = strftime('%m-%d', localtime())
 
-    class __Stream_Formatter(Formatter):
+    class _Stream_Formatter(Formatter):
         FORMATS = {20: '\033[1;32m[%(asctime)s] [%(levelname)s]\033[0m %(message)s',
                    30: '\033[1;33m[%(asctime)s] [%(levelname)s]\033[0m %(message)s',
                    40: '\033[1;31m[%(asctime)s] [%(levelname)s]\033[0m %(message)s'}
@@ -43,14 +59,14 @@ class Logger:
         def __init__(self, formats: dict = None, date_format: str = None):
             super().__init__()
             if formats is not None:
-                self.FORMATS[20] = formats[20] or self.FORMATS[20]
-                self.FORMATS[30] = formats[30] or self.FORMATS[30]
-                self.FORMATS[40] = formats[40] or self.FORMATS[40]
-            self.__date_format = date_format or '%m-%d %H:%M:%S'
+                self.FORMATS[20] = formats.get(20, None) or self.FORMATS[20]
+                self.FORMATS[30] = formats.get(30, None) or self.FORMATS[30]
+                self.FORMATS[40] = formats.get(40, None) or self.FORMATS[40]
+            self._date_format = date_format or '%m-%d %H:%M:%S'
 
         def format(self, record) -> str:
             log_fmt = self.FORMATS.get(record.levelno)
-            formatter = Formatter(log_fmt, self.__date_format)
+            formatter = Formatter(log_fmt, self._date_format)
             return formatter.format(record)
 
     def set_formatter(self, debug_format: str = None, info_format: str = None, warning_format: str = None,
@@ -71,44 +87,26 @@ class Logger:
         self._date_format = date_format or self._date_format
         self._format = debug_format or self._format
         formats = {20: info_format, 30: warning_format, 40: error_format}
-        self.__cmdh.setFormatter(self.__Stream_Formatter(formats, date_format))
-        self.__logh.setFormatter(Formatter(self._format, self._date_format))
+        self._cmdh.setFormatter(self._Stream_Formatter(formats, date_format))
+        self._logh.setFormatter(Formatter(self._format, self._date_format))
 
-    def __new_logh(self, str_time):
-        self.__logh = FileHandler('./log/{}/{}.log'.format(self.__bot_app_id, str_time), encoding='utf-8')
-        self.__logh.setFormatter(Formatter(self._format, self._date_format))
-        self.__logger.addHandler(self.__logh)
+    def _new_logh(self, str_time):
+        self._logh = FileHandler('./log/{}/{}.log'.format(self._bot_app_id, str_time), encoding='utf-8')
+        self._logh.setFormatter(Formatter(self._format, self._date_format))
+        self._logger.addHandler(self._logh)
 
-    def __check_date(self):
-        str_time = strftime('%m-%d', localtime())
-        if str_time != self.__previous_time:
-            self.__logger.removeHandler(self.__logh)
-            self.__new_logh(str_time)
-
+    @_check_date
     def debug(self, msg):
-        self.__check_date()
-        try:
-            self.__logger.debug(msg)
-        except UnicodeDecodeError:
-            pass
+        self._logger.debug(msg)
 
+    @_check_date
     def info(self, msg):
-        self.__check_date()
-        try:
-            self.__logger.info(msg)
-        except UnicodeDecodeError:
-            pass
+        self._logger.info(msg)
 
+    @_check_date
     def warning(self, msg):
-        self.__check_date()
-        try:
-            self.__logger.warning(msg)
-        except UnicodeDecodeError:
-            pass
+        self._logger.warning(msg)
 
+    @_check_date
     def error(self, msg):
-        self.__check_date()
-        try:
-            self.__logger.error(msg)
-        except UnicodeDecodeError:
-            pass
+        self._logger.error(msg)
--- qg_botsdk/qg_bot.py
+++ qg_botsdk/qg_bot.py
@@ -20,7 +20,7 @@ system("")
 reply_model = ReplyModel()
 retry = Retry(total=4, connect=3, backoff_factor=0.5)
 adapter = HTTPAdapter(max_retries=retry)
-version = '2.2.4'
+version = '2.2.5'
 pid = getpid()
 print(f'本次程序进程ID：{pid} | SDK版本：{version} | 即将开始运行机器人……')
 t_sleep(0.5)
@@ -99,7 +99,10 @@ class BOT:
     async def __time_event_check(self):
         while self.running:
             try:
-                self.__repeat_function()
+                if self.is_async:
+                    await self.__repeat_function()
+                else:
+                    self.__repeat_function()
             except Exception as error:
                 self.logger.error(error)
                 self.logger.debug(exception_handler(error))
