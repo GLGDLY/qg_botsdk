3021b912024563e5d4ba62bb7cf7457c0ffebdcc
GLGDLY <tzlgdly@gmail.com>
2022-08-15 21:18:38 +0800

    update docs

--- docs/conf.py
+++ docs/conf.py
@@ -12,7 +12,7 @@ project = 'qg_botsdk'
 copyright = '2022, GLGDLY'
 author = 'GLGDLY'
 version = 'latest'
-release = version
+release = 'latest'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
@@ -26,11 +26,16 @@ source_suffix = {'.rst': 'restructuredtext', '.md': 'markdown'}
 myst_footnote_transition = False
 
 language = 'zh_CN'
+show_authors = True
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
+html_title = 'qg_botsdk 帮助文档'
+html_logo = '_static/robot.png'
+html_favicon = '_static/favicon.ico'
 html_show_copyright = True
-htmlhelp_basename = "qg_botsdk_doc"
+html_show_sphinx = False
+html_search_language = ['zh', 'en']
